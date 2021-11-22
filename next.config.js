8fdb899354e69d5b2cc92e50c3c32ac468ea5485
biio-front <bohwajung@naver.com>
2021-11-22 14:58:35 +0900

    Add config for api

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isDevelopment = process.env.NODE_ENV !== 'production';
+const rewritesConfig = isDevelopment
+  ? [{ source: '/pokemon/:search', destination: 'https://pokeapi.co/api/v2/pokemon/:search' }]
+  : [];
+
 module.exports = {
   reactStrictMode: true,
-}
+  rewrites: async () => rewritesConfig,
+};
