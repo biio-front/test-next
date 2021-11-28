292b43a10d1055b755e57af1735280354575bd2f
biio-front <bohwajung@naver.com>
2021-11-28 21:23:26 +0900

    Add proxy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const isDevelopment = process.env.NODE_ENV !== 'production';
 const rewritesConfig = isDevelopment
-  ? [{ source: '/:path*', destination: 'https://pokeapi.co/api/v2/:path*' }]
+  ? [
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/:path*',
+        destination: 'https://pokeapi.co/api/v2/:path*',
+      },
+    ]
   : [];
 
 module.exports = {
